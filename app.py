bda521cb9b44a8beed37ba87dbbbe0f741b8c002
Usman Lari <usmanlari2000@gmail.com>
2025-07-03 11:09:33 +0500

    new commit

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ def whatsapp_bot():
     return str(twilio_response)
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
